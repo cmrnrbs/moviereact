6b8e694d79a65248fbe69af5c58a4ec1f1ed342b
Cemre Onur <cemrenurb@gmail.com>
2021-03-21 23:17:03 +0300

    part6 commit

--- app/components/TrailerItem.js
+++ app/components/TrailerItem.js
@@ -1,37 +1,51 @@
 import React from "react";
-import { View, Text, Image, Dimensions } from "react-native";
+import {
+  View,
+  Text,
+  Image,
+  Dimensions,
+  TouchableWithoutFeedback,
+} from "react-native";
+import { getYoutubeMeta } from "react-native-youtube-iframe";
 function TrailerItem(props) {
   const deviceWidth = Dimensions.get("window").width;
   const posterWidth = (deviceWidth - 50) / 2;
   const leftPosition = (posterWidth - 24) / 2;
-  console.log(posterWidth);
+  const marginValue = props.itemIndex % 2 == 0 ? 10 : 0;
+
+  getYoutubeMeta("sNhhvQGsMEc").then((meta) => {
+    console.log("title of the video : " + meta.title);
+  });
+
   return (
-    <View style={{ marginRight: 5 }}>
-      <Image
-        style={{
-          position: "absolute",
-          top: 38,
-          left: leftPosition,
-          zIndex: 1,
-          width: 24,
-          height: 24,
-        }}
-        source={require("../assets/play-button.png")}
-      />
-      <Image
-        resizeMode={"cover"}
-        style={{
-          width: posterWidth,
-          height: 100,
-          borderRadius: 20,
-          marginBottom: 5,
-        }}
-        source={{ uri: "http://image.tmdb.org/t/p/w342/" + props.poster }}
-      />
-      <Text style={{ flexWrap: "wrap", width: posterWidth }}>
-        {props.data.name}
-      </Text>
-    </View>
+    <TouchableWithoutFeedback onPress={props.onPressFunction}>
+      <View style={{ marginRight: marginValue, marginTop: 10 }}>
+        <Image
+          style={{
+            position: "absolute",
+            top: 38,
+            left: leftPosition,
+            zIndex: 1,
+            width: 24,
+            height: 24,
+          }}
+          source={require("../assets/play-button.png")}
+        />
+        <Image
+          resizeMode={"cover"}
+          style={{
+            width: posterWidth,
+            height: 100,
+            borderRadius: 20,
+            marginBottom: 5,
+          }}
+          source={{ uri: "http://image.tmdb.org/t/p/w342/" + props.poster }}
+        />
+        <Text style={{ flexWrap: "wrap", width: posterWidth }}>
+          {props.data.name}
+        </Text>
+      </View>
+    </TouchableWithoutFeedback>
   );
 }
 export default TrailerItem;
